e35e10908381b73bd43796a3c12b3661233aeb56
Nikhil More <nikhil.more@wissen.com>
2025-07-03 19:43:33 +0530

    intended bug

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Calculator {
     public int add(int a, int b) {
-        return a + b;
+        return a + b+1;
         //comment added for change
 
         //another change made
