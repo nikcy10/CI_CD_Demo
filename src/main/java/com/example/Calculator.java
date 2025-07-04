ab61e4b27ae687ed42266cad48a2d4ab618360ac
Nikhil More <nikhil.more@wissen.com>
2025-07-04 10:08:32 +0530

    extra comment

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,9 +4,10 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
         //comment added for change
-
+        
         //another change made
         //extra comment
+        //another extra comment
     }
 }
 
